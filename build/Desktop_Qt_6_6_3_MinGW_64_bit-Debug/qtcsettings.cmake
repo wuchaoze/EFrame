6015e32ce5d55309921b0ac6e5d498db485151fa
leon <wuchaoze@163.com>
2025-01-21 23:54:53 +0800

    Initial commit

---
+++ build/Desktop_Qt_6_6_3_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "F:/Qt/Qt6.7/Tools/mingw1120_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "F:/Qt/Qt6.7/6.6.3/mingw_64" CACHE "PATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "F:/Qt/Qt6.7/6.6.3/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/Document/Qt/EFrame/build/Desktop_Qt_6_6_3_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "F:/Qt/Qt6.7/Tools/mingw1120_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
